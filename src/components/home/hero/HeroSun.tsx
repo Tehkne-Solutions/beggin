fbf2cb621e04d8a39d7dd1665e0096d3aeb871b3
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-05-19 20:00:13 -0300

    fix: repair hero sun JSX after conflict merge

--- src/components/home/hero/HeroSun.tsx
+++ src/components/home/hero/HeroSun.tsx
@@ -24,7 +24,7 @@ export function HeroSun() {
       className="pointer-events-none absolute right-[11%] top-[16%] z-[2] hidden h-[170px] w-[170px] lg:block"
       aria-hidden="true"
     >
-      <motion.div className="relative h-full w-full">
+      <div className="relative h-full w-full">
         <Image
           src={heroAssets.sun}
           alt=""
