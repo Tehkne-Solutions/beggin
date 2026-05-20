66699f656647db9e61a87c5efa1e340927609e0e
GIP HNK Dev Team <dev@gip-hnk.com>
2026-05-20 16:54:35 -0300

    fix: awardSeal

--- src/components/home/awards/AwardSeal.tsx
+++ src/components/home/awards/AwardSeal.tsx
@@ -13,7 +13,7 @@ export function AwardSeal({ title, image }: AwardSealProps) {
   return (
     <article className="group relative flex shrink-0 items-center justify-center">
       <div
-        className="relative h-full w-full overflow-hidden rounded-full"
+        className="relative h-full w-full"
         style={{ height: sealSize, width: sealSize }}
       >
         <Image
@@ -23,7 +23,6 @@ export function AwardSeal({ title, image }: AwardSealProps) {
           className="object-contain"
           sizes={sizeStr}
         />
-        <span className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle_at_top_left,rgba(255,255,255,0.08),transparent_38%)] opacity-0 transition duration-300 group-hover:opacity-100" />
       </div>
     </article>
   );
