adbae8847d38ecb110f85ce7669011bf49c79386
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-05-21 17:45:59 -0300

    feat: stagger product grid cards

--- src/components/products/ProductGrid.tsx
+++ src/components/products/ProductGrid.tsx
@@ -4,8 +4,8 @@ import { ProductCard } from './ProductCard';
 export function ProductGrid(props: { products: Product[] }) {
   return (
     <div className="grid gap-8 md:grid-cols-2 xl:grid-cols-3">
-      {props.products.map((product) => (
-        <ProductCard key={product.slug} product={product} />
+      {props.products.map((product, index) => (
+        <ProductCard key={product.slug} product={product} index={index} />
       ))}
     </div>
   );
